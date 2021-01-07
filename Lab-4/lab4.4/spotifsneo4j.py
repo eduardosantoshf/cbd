ba81fda7fc48967dcb584c2b515962abd2020a1b
Eduardo Santos <eduardosantoshf@gmail.com>
2021-01-07 18:57:04 +0000

    Can't connect to the neo4j server

---
+++ Lab-4/lab4.4/spotifsneo4j.py
@@ -0,0 +1,34 @@
+from neo4j import GraphDatabase
+
+class SpotifsNeo4j:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth = (user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def insert_db(self):
+        with self.driver().session as session:
+            print("Inserting data set")
+            session.run(
+                "load csv with headers from 'file:///spotify_dataset/data_by_genres.csv' as row merge (g:Genre {genre: row.genres}) merge (k:Key {key: row.key}) merge (p:Popularity {popularity: row.popularity})"
+            )
+            print("Data set sucessfully inserted!")
+
+    def insert_relations_between_nodes(self):
+        print("Creating relations")
+        session.run(
+            "load csv with headers from 'file:///spotify_dataset/data_by_genres.csv' as line match (g:Genre {genre: row.genres}) match (k:Key {key: row.key}) match (p:Popularity {popularity: row.popularity}) merge (genre)-[:HAS_KEY]-(key) merge (genre)-[:HAS_POPULARITY]-(popularity)"
+        )
+        print("Relations sucessfully created!")
+
+if __name__ == "__main__":
+    connecter = SpotifsNeo4j("bolt://localhost:7687", 
+                            "neo4j", 
+                            "password"
+                )
+    connecter.insert_nodes()
+    connecter.insert_relations_between_nodes()
+    connecter.close()
+
+    #with open("../CBD_L44_output.txt", "w") as printer:
